f81ea497feba94b8a917df3b3750d28d3d98a050
John Doe <Jacksonzoo@users.noreply.github.com>
2024-12-06 15:31:05 -0500

    Completed Day 3 Part 2 of Advent Code

--- day3/day3.py
+++ day3/day3.py
@@ -2,7 +2,6 @@
 
 with open("input.txt", "r") as file:
     memory = file.read()
-print(memory)
 
 results = 0
 
@@ -34,6 +33,52 @@ for i in range(n):
             if 1 <= len(number2) <= 3 and memory[j] == ")":
                 results += int(number1) * int(number2)
 
-# if they do, multiply their x and y values and add them to a 'results' counter.
+# if they do, multiply their x and y values and add them to a 'results' counter
 print(results)
 
+# for part 2 we introduce new instructions: "do()"s and "don't()"s
+# when we encounter a "do()", this will enable the code to process mul(x,y)
+# when we encounter a "don't()", this will disable the code to process mul(x,y) even if it's correctly formatted
+with open("input.txt", "r") as file:
+    new_memory = file.read()
+
+new_results = 0
+enabler = True
+n = len(new_memory)
+
+for i in range(n):
+    if new_memory[i:i+4] == "do()":
+        enabler = True
+    elif new_memory[i:i+7] == "don't()":
+        enabler = False
+
+    if enabler and new_memory[i:i+4] == "mul(":
+        j = i + 4
+        number1 = ""
+        comma = False
+        for k in range(j, n):
+            if new_memory[k].isdigit():
+                number1 += new_memory[k]
+            elif new_memory[k] == ',':
+                comma = True
+                j = k + 1
+                break
+            else:
+                number1 = ""
+                break
+        if comma and 1 <= len(number1) <= 3:
+            number2 = ""
+            paranthesis = False
+            for l in range(j, n):
+                if new_memory[l].isdigit():
+                    number2 += new_memory[l]
+                elif new_memory[l] == ")":
+                    paranthesis = True
+                    break
+                else:
+                    number2 = ""
+                    break
+            if 1 <= len(number2) <= 3 and paranthesis:
+                if enabler:
+                    new_results += int(number1) * int(number2)
+print(new_results)
