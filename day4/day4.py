7ff8306bf7e6b55f6fac2aec1aebbe1ba5b9d633
John Doe <Jacksonzoo@users.noreply.github.com>
2024-12-07 14:35:24 -0500

    Appended the correct input information to the respective text files and included a brief instruction in day4.py

---
+++ day4/day4.py
@@ -0,0 +1,6 @@
+# open the necessary file we are trying to solve for Day 4
+
+
+# search the problem and record every time 'XMAS' had appeared
+# 'XMAS' could appear vertically, horizontally, diagonally, written backwards, or even overlapping (X for example)
+# return the amount of times 'XMAS' was found in those conditions
