e3f04da313ada4587e0be76c11b9c1337fa7845f
John Doe <Jacksonzoo@users.noreply.github.com>
2024-12-07 20:06:50 -0500

    Completed Day4 Part 2 of Advent Code

--- day4/day4.py
+++ day4/day4.py
@@ -44,3 +44,39 @@ for i in range(len(puzzle)):
 
 
 print(xmas_count)
+
+# For part 2 of the code, we are informed that the word search was not an 'XMAS' search; but an X-MAS search
+# The task is now to search the puzzle to find 'MAS' in the shape of an X; where 'MAS' can be forward or backward
+
+mas_count = 0
+
+# For reference
+    # (1, -1),  # down-left      diagonal-down-left
+    # (1, 1),   # down-right     diagonal-down-right
+    # (-1, 1),  # up-right       diagonal-up-right
+    # (-1, -1)  # up-left        diagona-up-left
+
+
+for i in range(len(puzzle)):
+    for j in range(len(puzzle[i])):
+        if puzzle[i][j] != "A":
+            continue
+        
+        diagonal1 = False       #checking for \ diagonal
+        if (0 <= i - 1 and 0 <= j - 1 and i + 1 < len(puzzle) and j + 1 < len(puzzle[0])):
+            char1 = puzzle[i - 1][j - 1]    #looking at up-left
+            char3 = puzzle[i + 1][j + 1]    #looking at down-right
+            if (char1 == "M" and char3 == "S") or (char1 == "S" and char3 == "M"):
+                diagonal1 = True
+        
+        diagonal2 = False       #checking for / diagonal
+        if (0 <= i - 1 and 0 <= j - 1 and i + 1 < len(puzzle) and j + 1 < len(puzzle[0])):
+            char1 = puzzle[i - 1][j + 1]    #looking at up-right
+            char3 = puzzle[i + 1][j - 1]    #looking at down-left
+            if (char1 == "M" and char3 == "S") or (char1 == "S" and char3 == "M"):
+                diagonal2 = True
+            
+        if diagonal1 and diagonal2:
+            mas_count += 1
+
+print(mas_count)
