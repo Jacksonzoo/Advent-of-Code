d4521dc744fc4cb36ad10abf8e969d100ff74054
John Doe <Jacksonzoo@users.noreply.github.com>
2024-12-09 10:12:54 -0500

    Added basic instructions and though process on how to approach Day 5 Part 1 Advent Code

---
+++ day5/day5.py
@@ -0,0 +1,7 @@
+# Open the input file and find a way to section the 'rules' and 'updates' so that we can differentiate the two
+
+
+
+# The goal is to look at the updats and see if they follow the 'rules' (X|Y) where page X at some point comes before page Y
+# Disregarding the rules that do not apply, if the updates abide by the relevant rules record the middle page number
+# Return the sum of all the middle page numebrs of the correctly ordered and rule abiding updates
