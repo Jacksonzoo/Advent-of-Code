278521fc6ea1bba366a0e36d0baf312302ec8e8a
John Doe <Jacksonzoo@users.noreply.github.com>
2024-12-09 11:25:51 -0500

    Added the necessary code to approach the problem

--- day5/day5.py
+++ day5/day5.py
@@ -1,6 +1,6 @@
 # Open the input file and find a way to section the 'rules' and 'updates' so that we can differentiate the two
 with open("testinput.txt", "r") as file:
-    rules = []
+    rules_temp = []
     updates = []
     section = 1
     for line in file:
@@ -8,9 +8,11 @@ with open("testinput.txt", "r") as file:
         if not line:
             section = 2
         elif section == 1:
-            rules.append(line)
+            rules_temp.append(line)
         else:
             updates.append(line)
+    
+    rules = [tuple(item.split("|")) for item in rules_temp]
 
 print(f"Rules:", rules)
 print(f"Updates:", updates)
@@ -20,5 +22,23 @@ print(f"Updates:", updates)
 # Disregarding the rules that do not apply, if the updates abide by the relevant rules record the middle page number
 # Return the sum of all the middle page numebrs of the correctly ordered and rule abiding updates
 
+total_pages = 0
 
+for before, after in rules:
+    for pages in updates:
+        correct_order = True
+        page = pages.split(",")
+        if before in page and after in page:
+            before_index = page.index(before)
+            after_index = page.index(after)
+            if before_index < after_index:
+                continue
+            else:
+                correct_order = False
+                break
+            print(correct_order)
+        if correct_order:
+            middle_page = int(pages[len(pages) // 2])
+            total_pages += middle_page
 
+print(total_pages)
