7b2f40d9df0957ca7ea989e16899efbb9070d944
John Doe <Jacksonzoo@users.noreply.github.com>
2024-12-10 10:20:05 -0500

    Generated a general idea of how the code should be as comments

---
+++ day6/day6.py
@@ -0,0 +1,9 @@
+# Start by opening the input file and splitting the lines to create your grid
+
+
+
+# Start by initializing a counter to 0 and adding a list of the necessary directions the guard can move
+# Find the guard in the grid and make not of its position
+# Create a while loop that will impose certain instructions depending on the position of the guard
+# If the guard reaches an obstacle "#", position the guard to make a 90 degree right turn and proceed
+# The guard leaves an "X" at every position it has been in, when the guard exits the grid, return the sum of "X"s
