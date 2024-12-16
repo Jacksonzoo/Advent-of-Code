fb5d9aea446444ddcf411ab9bb0f3ae853988178
John Doe <Jacksonzoo@users.noreply.github.com>
2024-12-16 12:41:42 -0500

    Commented initial thoughts to get started on Part 1 Day 9 Advent Code

---
+++ day9/day9.py
@@ -0,0 +1,10 @@
+# Open the file into a simple array
+
+
+
+# I want to parse through each number and assign it an "ID" value i < len array i ++
+# Next divide each number by alternating between them as file then free space
+# Create a new array to store the number of block files for each ID and "." number of free spaces
+# Then move file blocks one at a time from the end of the array to the first free space in the array
+# Each file block position would then need to be multiplied by the ID of the file; incremented in a counter
+# Return the sum
