0fd0ead83e1e46cb490ff60d1c6201e7ed53cfc0
John Doe <Jacksonzoo@users.noreply.github.com>
2024-12-16 11:57:00 -0500

    Completed all parts of Day 8 Advent Code *

--- day8/day8.py
+++ day8/day8.py
@@ -1,4 +1,5 @@
 # Open the file as a grid, splitting the lines into rows, the rows into columns
+from math import gcd
 
 with open("input.txt", "r") as file:
     grid = [list(line.strip()) for line in file]
@@ -44,3 +45,52 @@ for char, location in character.items():
 print(len(antinode))
 
 
+# For Part 2 we need to incorporate resonant harmonics
+# For all the frequencies that are aligned, they resonate with each other to create
+# more antinodes
+# The goal is to now find all new possible antinodes
+
+rows = len(grid)
+cols = len(grid[0]) if rows > 0 else 0
+
+antinode_p2 = set()
+
+character_positions = {}
+for r in range(rows):
+    for c in range(cols):
+        ch = grid[r][c]
+        if ch != '.':
+            if ch not in character_positions:
+                character_positions[ch] = []
+            character_positions[ch].append((r, c))
+
+for char, location in character_positions.items():
+    if len(location) < 2:
+        continue
+
+    for i in range(len(location)):
+        for j in range(i + 1, len(location)):
+            r1, c1 = location[i]
+            r2, c2 = location[j]
+
+            dr = r2 - r1
+            dc = c2 - c1
+
+            step_gcd = gcd(dr, dc)
+            dr //= step_gcd
+            dc //= step_gcd
+
+            rr, cc = r1, c1
+
+            while 0 <= rr < rows and 0 <= cc < cols:
+                antinode_p2.add((rr, cc))
+                rr += dr
+                cc += dc
+
+            rr, cc = r1, c1
+
+            while 0 <= rr < rows and 0 <= cc < cols:
+                antinode_p2.add((rr, cc))
+                rr -= dr
+                cc -= dc
+print(len(antinode_p2))
