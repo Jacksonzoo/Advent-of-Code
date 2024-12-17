9785c0ddd9d6e05377025923d09794467f971e97
John Doe <Jacksonzoo@users.noreply.github.com>
2024-12-16 21:04:11 -0500

    Finished all parts of Day 9 Advent Code with abundant amount of external help

--- day9/day9.py
+++ day9/day9.py
@@ -52,3 +52,66 @@ for index in range(len(block)):
         checksum += value
 
 print(checksum)
+
+
+# For part 2, we would like to compact the disk in a way where we do not have so many free space
+# We will now be moving the files in groups of ID into the availiable free space as opposed to one by one
+
+block_two = []
+checksum_two = 0
+
+file_id = 0
+for i in range(len(disk)):
+    if i % 2 == 0:
+        for j in range(int(disk[i])):
+            block_two.append(file_id)
+        file_id += 1
+    else:
+        for j in range(int(disk[i])):
+            block_two.append(".")
+
+for file_id in sorted(set(x for x in block_two if x != "."), reverse=True):
+    file_location = [idx for idx, val in enumerate(block_two) if val == file_id]
+    if not file_location:
+        continue
+
+    file_start = min(file_location)
+    file_size = len(file_location)
+
+    free_space = []
+    free = None
+
+    for i in range(len(block_two)):
+        if block_two[i] == ".":
+            if free is None:
+                free = i
+        else:
+            if free is not None:
+                length = i - free
+                free_space.append((free, length))
+                free = None
+
+    if free is not None:
+        length = len(block_two) - free
+        free_space.append((free, length))
+
+
+    moved = False
+    for free, length in free_space:
+        if length >= file_size and (free + file_size) <= file_start:
+            for location in file_location:
+                block_two[location] = "."
+
+            for j in range(file_size):
+                block_two[free + j] = file_id
+
+            moved = True
+            break
+
+for index in range(len(block_two)):
+    if block_two[index] != ".":
+        value = block_two[index] * index
+        checksum_two += value
+
+
+print(checksum_two)
