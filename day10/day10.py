60e08258cb35ca671c6e1f1521a4588cc8c1d776
John Doe <Jacksonzoo@users.noreply.github.com>
2024-12-17 08:56:06 -0500

    Filled in the corresponding input files and made comments on initial thoughts

---
+++ day10/day10.py
@@ -0,0 +1,12 @@
+# Open the file as a grid so we get our 'topographic map'
+
+
+
+
+# An optimal hiking path is one where we start at height 0 and reach heigh 9 gradually increasing by 1
+# A trailhead is the start of the trail, height of 0
+# Trailhead score is the number of positions at height of 9 we can reach via an optimal path
+# Find the trailscore for each trailhead and return the sum
+
+
+
