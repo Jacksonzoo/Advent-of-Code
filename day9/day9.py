c52f714fa086c8ae310bb72a74814c2bb0ce5b63
John Doe <Jacksonzoo@users.noreply.github.com>
2024-12-16 19:13:11 -0500

    Completed Day 9 Part 1 Advent Code

--- day9/day9.py
+++ day9/day9.py
@@ -1,5 +1,7 @@
 # Open the file into a simple array
 
+with open("input.txt", "r") as files:
+    disk = files.read()
 
 
 # I want to parse through each number and assign it an "ID" value i < len array i ++
@@ -8,3 +10,45 @@
 # Then move file blocks one at a time from the end of the array to the first free space in the array
 # Each file block position would then need to be multiplied by the ID of the file; incremented in a counter
 # Return the sum
+
+checksum = 0
+block = []
+file_id = 0
+
+for i in range(len(disk)):
+    if i % 2 == 0:
+        for j in range(int(disk[i])):
+            block.append(file_id)
+        file_id += 1
+    else:
+        for j in range(int(disk[i])):
+            block.append(".")
+
+
+while True:
+    free_index = None
+
+    for index in range(len(block)):
+        if block[index] == ".":
+            if any(value != "." for value in block[index + 1:]):
+                free_index = index
+                break
+
+    if free_index is None:
+        break
+
+    last_file = None
+    for index in range(len(block) -1, -1, -1):
+        if block[index] != ".":
+            last_file = index
+            break
+
+    file_block = block.pop(last_file)
+    block[free_index] = file_block
+
+for index in range(len(block)):
+    if block[index] != ".":
+        value = block[index] * index
+        checksum += value
+
+print(checksum)
