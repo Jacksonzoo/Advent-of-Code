830c4087b162aef61f0050e80808e1b49796e3c1
John Doe <Jacksonzoo@users.noreply.github.com>
2024-12-18 16:51:44 -0500

    Completed Day 10 Part 1 of Advent Code

--- day11/day11.py
+++ day11/day11.py
@@ -1,7 +1,10 @@
 # Open the input file as a list
 
-
-
+def open_stone_line(filename):
+    with open(filename, "r") as file:
+        stones = file.readline().split()
+        arrangement = [int(rock) for rock in stones]
+    return arrangement
 
 
 # Given an arrangement of stones, each with an engraved number. 
@@ -12,3 +15,43 @@
 # for example: the number 1234 is engraved. The stone is split where the 12 is engraved to the left stone and 34 engraved on the right
 # If none of the rules is applicable to the number engraved, multiply that number by 2024
 # After 25 blinks, how many stones do we have in this arrangement
+
+def split_digit(stone_number):
+    stone_str = str(stone_number)
+    middle = len(stone_str) // 2
+
+    left_half = int(stone_str[:middle])
+    right_half = int(stone_str[middle:])
+
+    return left_half, right_half
+
+def apply_rule(arrangement):
+    new_arrangement = []
+    for stone in arrangement:
+        if stone == 0:
+            new_arrangement.append(1)
+        elif len(str(stone)) % 2 == 0:
+            left_stone, right_stone = split_digit(stone)
+            new_arrangement.extend([left_stone, right_stone])
+        else:
+            new_arrangement.append(stone * 2024)
+    return new_arrangement
+
+def blinking_25_times(arrangement):
+    blink = 0
+    while blink < 25:
+        arrangement = apply_rule(arrangement)
+        blink += 1
+    return arrangement
+
+def total_stones(arrangement):
+    total = len(blinking_25_times(arrangement))
+    return total
+
+
+
+stone_arrangement = open_stone_line("input.txt")
+
+print(total_stones(stone_arrangement))
+
+
