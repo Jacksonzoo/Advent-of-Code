b523463cf68a7c06b9a0711b928dfb5655fee25a
John Doe <Jacksonzoo@users.noreply.github.com>
2024-12-22 18:24:20 -0500

    'Finished' Day 12 Part 2, Completed Day 12 Advent Code

--- day12/day12.py
+++ day12/day12.py
@@ -68,6 +68,74 @@ total_cost = find_fence_cost(regions)
 print(total_cost)
 
 
+# Part 2, a discount is introduced for buying the fences in bulk. Instead of using the 
+# perimeter when considering the price we can now consider the number of sides the regions have.
+# Consider a 2x2 grid. As opposed to having a fence cost of $32 (perimeter (8) * area (4))
+# we now have a fence price of $16 (side (4) * area (4))
 
+
+def get_corners(x, y, garden, plant_type):
+    directions = [
+        (-1, -1,), (-1, 0), (-1, 1),    # NW, N, NE
+        (0, -1),            (0, 1),     # W,     E
+        (1, -1),  (1, 0),   (1, 1)      # SW, S, SE
+    ]
+
+    adjacent = [
+        check_in_garden(x + dx, y+ dy, garden) and garden[x + dx][y + dy] == plant_type
+        for dx, dy in directions
+    ]
+
+    NW, N, NE, W, E, SW, S, SE = adjacent
+    return sum([
+        N and W and not NW, 
+        N and E and not NE,
+        S and W and not SW,
+        S and E and not SE,
+        not (N or W),
+        not (N or E),
+        not (S or W),
+        not (S or E)
+    ])
+
+def find_region(x, y, garden):
+    plant_type = garden[x][y]
+    region = set()
+    queue = [(x, y)]
+
+    while queue:
+        cx, cy = queue.pop()
+        if (cx, cy) in region:
+            continue
+        region.add((cx, cy))
+
+        for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+            nx, ny = cx + dx, cy + dy
+            if(
+                check_in_garden(nx, ny, garden)
+                and garden[nx][ny] == plant_type
+                and (nx, ny) not in region
+                and (nx, ny) not in queue
+            ):
+                queue.append((nx, ny))
+
+    corners = sum(get_corners(rx, ry, garden, plant_type) for rx, ry in region)
+    return region, corners * len(region)
+
+def calc_discounted_cost(garden):
+    visited = set()
+    total_cost = 0
+
+    for x in range(len(garden)):
+        for y in range(len(garden[0])):
+            if (x, y) not in visited:
+                region, cost = find_region(x, y, garden)
+                total_cost += cost
+                visited |= region
+    return total_cost
+
+garden_map_two = open_garden_map("input.txt")
+discounted_cost = calc_discounted_cost(garden_map_two)
+print(discounted_cost)
 
 
