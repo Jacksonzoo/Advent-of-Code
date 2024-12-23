1b6ee85860584c66eeeeb92db7dc6bacf54c8373
John Doe <Jacksonzoo@users.noreply.github.com>
2024-12-23 11:11:20 -0500

    Commented initial instructions to gather intiial thoughts on how to start

---
+++ day13/day13.py
@@ -0,0 +1,15 @@
+# Open the input file as button instructions and answer
+
+
+
+
+# Part 1 describes a claw magine arcade game, where instead of joystick we have 2 buttons "A" and "B"
+# Assuming we start at (0, 0) on a grid, we need to reach the prize at location (x, y)
+# Button A costs 3 tokens to use and moves along the X and Y axis a certain amount
+# Button B costs 1 token to use and also moves along the X and Y axis a certain amount
+# Goal is to find the least amount of tokens used to win as many prizes as possible
+
+
+
+
+
