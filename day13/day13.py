a72f8e430adc6a9f87d5c3b45e1071257d6bea66
John Doe <Jacksonzoo@users.noreply.github.com>
2024-12-24 13:19:31 -0500

    Reframed open input function for readability

--- day13/day13.py
+++ day13/day13.py
@@ -3,39 +3,51 @@
 def open_instructions(filename):
     arcade = []
     with open(filename, "r") as file:
-        lines = [line.strip() for line in file]
+        lines = [line.strip() for line in file if line.strip()]
 
-        button_a = None
-        button_b = None
-        prize = None
+        button_a, button_b, prize = None, None, None
 
         for line in lines:
             if line.startswith("Button A"):
-                ax, ay = map(int, [line.split()[2].split('+')[1], line.split()[3].split('+')[1]])
+                x = line.split()[2]
+                y = line.split()[3]
+                ax = int(x.split('+')[1].strip(','))
+                ay = int(y.split('+')[1])
                 button_a = (ax, ay)
             elif line.startswith("Button B"):
-                bx, by = map(int, [line.split()[2].split('+')[1], line.split()[3].split('+')[1]])
+                x = line.split()[2]
+                y = line.split()[3]
+                bx = int(x.split('+')[1].strip(','))
+                by = int(y.split('+')[1])
                 button_b = (bx, by)
             elif line.startswith("Prize"):
-                prize_x, prize_y = map(int, [line.split()[1].split('=')[1], line.split()[2].split('=')[1]])
-                prize = (prize_x, prize_y)
+                x_prize = int(line.split()[1].split('=')[1].strip(','))
+                y_prize = int(line.split()[2].split('=')[1])
+                prize = (x_prize, y_prize)
 
                 if button_a and button_b and prize:
-                    machine = {'Button A': button_a, 'Button B': button_b, 'Prize': prize}
-                    arcade.append(machine)
+                    arcade.append({
+                        'Button A': button_a,
+                        'Button B': button_b,
+                        'Prize': prize
+                    })
 
                     button_a, button_b, prize = None, None, None
     return arcade
                                        
             
-
-
 # Part 1 describes a claw magine arcade game, where instead of joystick we have 2 buttons "A" and "B"
 # Assuming we start at (0, 0) on a grid, we need to reach the prize at location (x, y)
 # Button A costs 3 tokens to use and moves along the X and Y axis a certain amount
 # Button B costs 1 token to use and also moves along the X and Y axis a certain amount
 # Each button should be pressed no more than 100 times to win a prize
 # Goal is to find the least amount of tokens used to win as many prizes as possible
+
+
+
+
+
+
 
 
 
