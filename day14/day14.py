8eb5fd71a99b88d78fcbceea4fce9e8191548611
John Doe <Jacksonzoo@users.noreply.github.com>
2024-12-26 09:33:48 -0500

    Inserted the corresponding file inputs and commented initial starting process

---
+++ day14/day14.py
@@ -0,0 +1,17 @@
+# Open the input files as usable instrucitons
+
+
+
+
+
+
+
+# Part 1, given a list of instructions of robot positions (p) and how they move along a tile each second (v),
+# the goal is to find a safety score which is obtained by dividing the grid into 4 equal quadrants and
+# multiplying how many robots are in each quadrant by each other
+# Note that if robots move 'outside' the grid, they essentially move to the other side as if it were an extended grid
+# Robots can also overlap with each other
+
+
+
+
