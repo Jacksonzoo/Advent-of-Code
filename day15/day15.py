51027aa310b196cde68ad447ffef01c5f94f608c
John Doe <Jacksonzoo@users.noreply.github.com>
2024-12-28 16:45:28 -0500

    Commented inital thought processes to start Day 15 Advent Code

---
+++ day15/day15.py
@@ -0,0 +1,16 @@
+# Open the file returning a map and a list of directional instructions
+
+
+
+
+
+
+
+# Part 1, tasked to find the total sum of all boxes gps coordinates (100 * distance from top edge + left edge of map)
+# Robot (@) can push boxes (O) as long as the other side is not a wall (#)
+# After the robot moves in order of the sequence in directional instructions, calculate the sum of all box gps coordinate locations
+
+
+
+
+
